f9a54ac6b97c4439ac83570f2511d846b0184f4c
Rayane-Nait-Saidi <nr_nait_saidi@esi.dz>
2025-11-28 20:45:58 +0100

    Added Web 1.0 section to the project

---
+++ src/web1/PageSix.jsx
@@ -0,0 +1,31 @@
+import background1 from '../assets/background1.jpg'
+function PageSix(){
+    return(
+        <div className='big6' style={{backgroundImage:`url(${background1})`}}>
+           <div className="title6">
+            <h1>5.Advs&&Disadvs</h1>
+           </div>
+            <div className="content6">
+                           <div className="adr6">
+                               <h1>Advantages and Disadvantages</h1>
+                           </div>
+                           <div className="explain6">
+                               <h1 style={{color:"white"}}>Advantages</h1>
+                               <li>Simple</li>
+                               <li>Fast</li>
+                               <li>Secure and easy to host</li>
+                               <h1 style={{color:"white"}}>Disadvantages</h1>
+                               <li>No interactivity and poor design</li>
+                               <li>Hard to update</li>
+                               <li>No personalisation</li>
+                               <li>No user Content</li>
+                               <br /><br />
+                               <h1 id='alert'>Need to go to another kind of web</h1>
+                           </div>
+                           
+                       </div>
+        </div>
+    )
+}
+
+export default PageSix ;
